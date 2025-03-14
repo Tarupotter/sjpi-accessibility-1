990e60d450ce20a6baad404304675cdb1a0bdff9
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-03-14 10:46:23 +0100

    read more link

---
+++ script.js
@@ -0,0 +1,10 @@
+const readMoreLink = document.querySelector("#readMoreLink");
+const moreText = document.querySelector("#moreText");
+
+const toggleText = () => {
+  const isHidden = moreText.classList.contains("hidden");
+  moreText.classList.toggle("hidden", !isHidden);
+  readMoreLink.textContent = isHidden ? "Read less" : "Continue reading";
+};
+
+readMoreLink.addEventListener("click", toggleText);
